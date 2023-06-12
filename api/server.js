934b84413eff33e16f019f6aed3d7eb2edd86c74
serkantoraman <serkan@github.com>
2023-06-12 15:31:15 +0300

    server started  with http

--- api/server.js
+++ api/server.js
@@ -1,3 +1,18 @@
 // SUNUCUYU BU DOSYAYA KURUN
+// const http = require('http');
+
+// const hostname = '127.0.0.1'
+// const port = 3000;
+
+// const server =http.createServer((req,res)=>{
+//     res.statusCode = "200";
+//     res.setHeader('Content-Type,"text/plain');
+//     res.end('hello world from http server')
+// })
+
+// server.listen(port, ()=>{
+//   console.log(`Server running on port ${port}...`)
+// })
+
 
 module.exports = {}; // SERVERINIZI EXPORT EDİN {}
--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const server = require('./api/server');
 
-const port = 9000;
+const http = require('http');
+
+const hostname = '127.0.0.1'
+const port = 3000;
+
+const server =http.createServer((req,res)=>{
+    res.statusCode = "200";
+    res.setHeader('Content-Type','text/plain');
+    res.end('hello world from http server')
+})
+
+server.listen(port,hostname, ()=>{
+  console.log(`Server running on port ${port}...`)
+})
+
+
+
+
+// const server = require('./api/server');
+
+// const port = 9000;
 
 // START YOUR SERVER HERE
+
+
