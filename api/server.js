2675ceafaa8f9e45823a98768a13d4e76fbcdaff
serkantoraman <serkan@github.com>
2023-06-12 15:35:56 +0300

    server codes moved from index to server js

--- api/server.js
+++ api/server.js
@@ -1,18 +1,15 @@
 // SUNUCUYU BU DOSYAYA KURUN
-// const http = require('http');
 
-// const hostname = '127.0.0.1'
-// const port = 3000;
-
-// const server =http.createServer((req,res)=>{
-//     res.statusCode = "200";
-//     res.setHeader('Content-Type,"text/plain');
-//     res.end('hello world from http server')
-// })
-
-// server.listen(port, ()=>{
-//   console.log(`Server running on port ${port}...`)
-// })
+const http = require('http');
 
 
-module.exports = {}; // SERVERINIZI EXPORT EDİN {}
+const server =http.createServer((req,res)=>{
+    res.statusCode = "200";
+    res.setHeader('Content-Type','text/plain');
+    res.end('hello world from http server')
+})
+
+
+
+
+module.exports = server; // SERVERINIZI EXPORT EDİN {}
--- index.js
+++ index.js
@@ -1,26 +1,14 @@
 
-const http = require('http');
 
-const hostname = '127.0.0.1'
-const port = 3000;
+const server = require('./api/server');
 
-const server =http.createServer((req,res)=>{
-    res.statusCode = "200";
-    res.setHeader('Content-Type','text/plain');
-    res.end('hello world from http server')
-})
+const hostname = '127.0.0.1';
+const port = 9000;
+
+//START YOUR SERVER HERE
 
 server.listen(port,hostname, ()=>{
   console.log(`Server running on port ${port}...`)
 })
-
-
-
-
-// const server = require('./api/server');
-
-// const port = 9000;
-
-// START YOUR SERVER HERE
 
 
